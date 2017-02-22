3cec87913e1e5e1125dc5c582d00de9966bed2a1
Shubham Goel <mailshubhhere@gmail.com>
2017-02-22 18:44:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,67 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var students={
+shubham:{
+    name:'Shubham Goel',
+    dept:'Information Technology',
+    roll:'62',
+    colg:'Jadavpur University'
+},
+
+krishna:{
+    name:'Krishna Agarwal',
+    dept:'Computer Science',
+    roll:'24',
+    colg:'Kalyani Eng.'
+},
+
+imam:{
+    name:'Imam Khan',
+    dept:'Civil Engineering',
+    roll:'10',
+    colg:'Techno India'
+}
+};
+
+app.get('/ui/:varName', function (req, res) {
+   var name=students[varName[name]];
+ var colg=students[varName[colg]];
+ var dept=students[varName[dept]];
+ var roll=students[varName[roll]];
+  res.send(`<!doctype html>
+<html>
+    <head>
+        <title>
+            ${name}
+        </title>
+        <link href="style.css" rel='stylesheet'>
+    </head>
+    <body>
+        <div class='container' id='para' style='text:center'>
+            <h1>
+                Personal Credentials.
+            </h1>
+            <hr/>
+            <ul>
+                <li>
+                    Name:<span id='name'>${name}</span>
+                </li>
+                <li>
+                    College:<span id='college'>${colg}</span>
+                </li>
+                <li>
+                    Department:<span id='dept'>${dept}</span>
+                </li>
+                <li>
+                    Roll no. <span id='roll'>${roll}</span>
+                </li>
+            </ul>
+        </div>
+    </body>
+</html>
+`);
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
